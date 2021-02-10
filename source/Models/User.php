01db39dfbdf626d9a625cda940fb5fa291d824a1
whedev <whendel.morais.9@gmail.com>
2021-02-10 20:19:19 -0300

    adicionando metodo save

--- source/Models/User.php
+++ source/Models/User.php
@@ -43,11 +43,69 @@ class User extends Model
         return $this;
     }
 
+    /**
+     * @param string $email
+     * @param string $columns
+     * @return User|null
+     */
     public function findByEmail(string $email, string $columns = "*"): ?User
     {
         return $this->find("email = :email", "email={$email}", $columns)->fetch();
     }
 
+    /**
+     * @return bool
+     */
+    public function save(): bool
+    {
+        if (!$this->required()){
+            $this->message->warning("Por favor, preencha todos os dados e tente novamente!");
+            return false;
+        }
 
+        if (!is_email($this->email)){
+            $this->message->warning("O e-mail informado não tem um formato valido");
+            return false;
+        }
 
+        if (!is_passwd($this->password)){
+            $min = CONF_PASSWD_MIN_LEN;
+            $max = CONF_PASSWD_MAX_LEN;
+            $this->message->warning("Sua senha deve ter entre {$min} a {$max} caracteres");
+            return false;
+        }
+
+        $userId = $this->id;
+
+        // UPDATE
+        if (!empty($userId)){
+            if ($this->find("email = :email AND id != :id", "email={$this->email}&id={$userId}")){
+                $this->message->error("O email informado já está cadastrado!");
+                return false;
+            }
+
+            $this->update($this->safe(), "id = :id", "id={$userId}");
+            if ($this->fail()){
+                $this->message->error("Erro ao atualizar, verifique os dados e tente novamente. Caso o erro persista, contacte nosso suporte. ");
+                return false;
+            }
+        }
+
+        // CREATE
+        if (empty($userId)){
+            if ($this->findByEmail($this->email, "id")){
+                $this->message->error("O email informado já está cadastrado!");
+                return false;
+            }
+
+            $userId = $this->create($this->safe());
+            if ($this->fail()){
+                $this->message->error("Erro ao salvar, verifique os dados e tente novamente. Caso o erro persista, contacte nosso suporte. ");
+                return false;
+            }
+        }
+
+        $this->data = ($this->findById($userId))->data();
+        return true;
+    }
 }
