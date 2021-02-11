5e4507de254d1ef8166a657d29a59bb3c2853e38
whedev <whendel.morais.9@gmail.com>
2021-02-10 21:45:07 -0300

    adic. helpers de data

--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -314,8 +314,38 @@ function theme(string $path = null, string $theme = CONF_VIEW_THEME): string
     return CONF_URL_BASE."/themes/{$theme}";
 }
 
-
 // DATE
+
+/**
+ * @param string $date
+ * @param string $format
+ * @return string
+ * @throws Exception
+ */
+function date_fmt(string $date = "now", string $format = "d/m/Y H\hi"): string
+{
+    return (new DateTime($date))->format($format);
+}
+
+/**
+ * @param string $date
+ * @return string
+ * @throws Exception
+ */
+function date_fmt_br(string $date = "now"): string
+{
+    return (new DateTime($date))->format(CONF_DATE_BR);
+}
+
+/**
+ * @param string $date
+ * @return string
+ * @throws Exception
+ */
+function date_fmt_app(string $date = "now"): string
+{
+    return (new DateTime($date))->format(CONF_DATE_APP);
+}
 
 // CORE
 
