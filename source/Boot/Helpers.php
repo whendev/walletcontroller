f5d568f56e21227a5393188a50b0317c85a601fe
whedev <whendel.morais.9@gmail.com>
2021-02-10 21:29:21 -0300

    adic. helpers de string

--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -138,6 +138,104 @@ function request_repeat(string $field, string $value): bool
 
 // STRING
 
+/**
+ * @param string $string
+ * @return string
+ */
+function str_slug(string $string): string
+{
+    $string = filter_var(mb_strtolower($string), FILTER_SANITIZE_STRIPPED);
+    $formats = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]/?;:.,\\\'<>°ºª';
+    $replace = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr                                 ';
+
+    return str_replace(["-----", "----", "---", "--"], "-",
+        str_replace(" ", "-",
+            trim(strtr(utf8_decode($string), utf8_decode($formats), $replace))
+        )
+    );
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_studly_case(string $string): string
+{
+    $string = str_slug($string);
+    return str_replace(" ", "",
+        mb_convert_case(str_replace("-", " ", $string), MB_CASE_TITLE)
+    );
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_camel_case(string $string): string
+{
+    return lcfirst(str_studly_case($string));
+}
+
+/**
+ * @param string $string
+ * @return string
+ */
+function str_title(string $string): string
+{
+    return mb_convert_case(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS), MB_CASE_TITLE);
+}
+
+function str_textarea(string $text): string
+{
+    $text = filter_var($text, FILTER_SANITIZE_STRIPPED);
+    $arrayReplace = ["&#10;", "&#10;&#10;", "&#10;&#10;&#10;", "&#10;&#10;&#10;&#10;", "&#10;&#10;&#10;&#10;&#10;"];
+    return "<p>". str_replace($arrayReplace, "</p></p>", $text). "</p>";
+}
+
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_words(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    $arrWords = explode(" ", $string);
+    $numWords = count($arrWords);
+
+    if ($numWords < $limit) {
+        return $string;
+    }
+
+    $words = implode(" ", array_slice($arrWords, 0, $limit));
+    return "{$words}{$pointer}";
+}
+
+/**
+ * @param string $string
+ * @param int $limit
+ * @param string $pointer
+ * @return string
+ */
+function str_limit_chars(string $string, int $limit, string $pointer = "..."): string
+{
+    $string = trim(filter_var($string, FILTER_SANITIZE_SPECIAL_CHARS));
+    if (mb_strlen($string) <= $limit) {
+        return $string;
+    }
+
+    $chars = mb_substr($string, 0, mb_strrpos(mb_substr($string, 0, $limit), " "));
+    return "{$chars}{$pointer}";
+}
+
+function str_price(string $price): string
+{
+    return number_format($price, 2, ",", ".");
+}
+
+
 // URL
 
 // ASSETS
