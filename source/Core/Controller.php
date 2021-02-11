c1b51e68099e65750218da05db9ce18964fa9d30
whedev <whendel.morais.9@gmail.com>
2021-02-10 21:05:37 -0300

    criando view e message ao instanciar controller

--- source/Core/Controller.php
+++ source/Core/Controller.php
@@ -4,6 +4,9 @@
 namespace Source\Core;
 
 
+
+use Source\support\Message;
+
 /**
  * Class Controller
  * @package Source\Core
@@ -11,12 +14,17 @@ namespace Source\Core;
 class Controller
 {
 
+    protected View $view;
+    protected Message $message;
+
     /**
      * Controller constructor.
+     * @param string|null $pathToViews
      */
-    public function __construct()
+    public function __construct(string $pathToViews = null)
     {
-
+        $this->message = new Message();
+        $this->view = new View($pathToViews);
     }
 
 }
