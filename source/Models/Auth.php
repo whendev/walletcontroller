12be84de05793e62e723649cebacc12dd31b3a37
whedev <whendel.morais.9@gmail.com>
2021-02-12 15:58:17 -0300

    adic. register

--- source/Models/Auth.php
+++ source/Models/Auth.php
@@ -6,6 +6,8 @@ namespace Source\Models;
 
 use Source\Core\Model;
 use Source\Core\Session;
+use Source\Core\View;
+use Source\Support\Email;
 
 /**
  * Class Auth
@@ -21,6 +23,9 @@ class Auth extends Model
         parent::__construct("users", ["email", "password"], ["id"]);
     }
 
+    /**
+     * @return User|null
+     */
     public static function user(): ?User
     {
         $session = new Session();
@@ -28,6 +33,32 @@ class Auth extends Model
             return null;
         }
         return (new User())->findById($session->authUser);
+    }
+
+    /**
+     * @param User $user
+     * @return bool
+     */
+    public function register(User $user): bool
+    {
+        if(!$user->save()){
+            $this->message = $user->message;
+            return false;
+        }
+
+        $view = new View(__DIR__."/../../shared/views/email");
+        $message = $view->render("confirm", [
+            "first_name" => $user->first_name,
+            "confirm_link" => url("/obrigado/". base64_encode($user->email))
+        ]);
+
+        (new Email())->bootstrap("Ative sua conta no ". CONF_SITE_NAME,
+            $message,
+            $user->email,
+            "{$user->first_name} {$user->last_name}"
+        )->send();
+
+        return true;
     }
 
     /**
