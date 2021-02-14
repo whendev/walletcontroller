feacf2e4310078a5d95490fe969e29ebe9fc21b9
whedev <whendel.morais.9@gmail.com>
2021-02-14 00:09:57 -0300

    pequenas mudanças

--- source/Controllers/App.php
+++ source/Controllers/App.php
@@ -6,10 +6,13 @@ namespace Source\Controllers;
 
 use Source\Core\Controller;
 use Source\Models\Auth;
+use Source\Models\User;
 use Source\Support\Message;
 
 class App extends Controller
 {
+    private ?User $user;
+
     public function __construct()
     {
         parent::__construct(__DIR__."/../../themes/".CONF_VIEW_APP."/");
@@ -18,11 +21,13 @@ class App extends Controller
             (new Message())->warning("Você precisa efetuar o login primeiro")->flash();
             redirect("/entrar");
         }
+
+        $this->user = Auth::user();
     }
 
     public function home()
     {
-        echo "olá";
+        var_dump($this->user);
     }
 
 }
