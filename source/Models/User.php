3e2195b63b5aabb7df89e31a08a1abe7ed9f0661
whedev <whendel.morais.9@gmail.com>
2021-02-14 16:37:28 -0300

    adic. metodo photo

--- source/Models/User.php
+++ source/Models/User.php
@@ -54,6 +54,18 @@ class User extends Model
     }
 
     /**
+     * @return string|null
+     */
+    public function photo(): ?string
+    {
+        if ($this->photo && file_exists(__DIR__."/../../". CONF_UPLOAD_DIR . "/{$this->photo}")){
+            return $this->photo;
+        }
+
+        return null;
+    }
+
+    /**
      * @return bool
      */
     public function save(): bool
