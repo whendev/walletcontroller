a556e2f52f1612d82816287f3a7298c325847050
whedev <whendel.morais.9@gmail.com>
2021-02-14 00:02:45 -0300

    adic. rotas para confirmar cadastro e alterar senha

--- index.php
+++ index.php
@@ -16,6 +16,23 @@ $route->post("/entrar", "Web:login");
 $route->get("/cadastrar", "Web:register");
 $route->post("/cadastrar", "Web:register");
 
+$route->get("/recuperar", "Web:forget");
+$route->post("/recuperar", "Web:forget");
+
+$route->get("/recuperar/{code}", "Web:reset");
+$route->post("/recuperar/resetar", "Web:reset");
+
+// MAIL
+$route->get("/confirmar", "Web:confirm");
+$route->get("/obrigado/{email}", "Web:success");
+
+
+// APP
+$route->group("/app");
+$route->get("/", "App:home");
+
+
+
 /**
  * ROUTE
  */
